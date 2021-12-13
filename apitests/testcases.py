7c7eb31b3ac539da6e45d71634f8d662648cb71e
mcyan <michealyan@hotmail.com>
2021-12-13 18:44:23 +1100

    Updated to cover Leaderboard

--- apitests/testcases.py
+++ apitests/testcases.py
@@ -13,6 +13,9 @@ import reademail
 import datetime
 
 # the token is generated around 5pm 12/12/2021, will be expired in 3 days
+# the tests could be switched later to automatically generate and use new token for running testing script. Feature to read/return new token from gmail is already available in "reademail.py"
+# Reason not doing this right now, because if implemented, Python built-in class unittest.TestCase will generate a new token for every test cases
+# which may cause extra load on back-end server, also reading new token from gmail for every test cases can significantly slow down the tests.
 AUTH = r'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiJseTIwMjExMjEyIiwiZW1haWwiOiJsaS15YW5AaG90bWFpbC5jb20iLCJpYXQiOjE2MzkyODM2OTIsImV4cCI6MTYzOTU0Mjg5Mn0.2tuhJr97DPsm0dha7tEQlkl6XOUnMv0Nn-9-MGtmNgzk-px-n1EGjx7IwQK29-DWQpd5KMcKBNm2H_-TUIbUUg'
 
 BASEURL = r'https://supervillain.herokuapp.com'
@@ -20,6 +23,7 @@ GENTOKEN_URL = r'https://supervillain.herokuapp.com/auth/gentoken'
 VRYTOKEN_URL = r'https://supervillain.herokuapp.com/auth/verifytoken'
 LOGIN_URL = r'https://supervillain.herokuapp.com/auth/user/login'
 REGISTER_URL = r'https://supervillain.herokuapp.com/auth/user/register'
+USER_URL = r'https://supervillain.herokuapp.com/v1/user'
 
 #TO-DO: change to inherit from "unittest.TestCase" to enable testing
 class AppAuthGenerate(unittest.TestCase):    
@@ -626,6 +630,380 @@ class UserAuth(unittest.TestCase):
         assert str(r).lower().find(expected_code)>=0, "Error - should not allow login as password is incorrect! Response=" + str(r)
         #assert str(r.content).lower().find(expected_res)>=0, "Error - unexpected return message! Response=" + str(r.content)         
     
+    def tearDown(self):
+        pass
+
+
+
+#TO-DO: change to inherit from "unittest.TestCase" to enable testing
+class UserLeaderboard(unittest.TestCase):
+#class UserAuth():    
+    '''
+    Test User Leaderboard API
+    '''        
+    def setUp(self):
+        #TO-DO: if time allowed, should change to get new token via email (function is already available)        
+        self.body = {}
+        
+        self.g_headers = {
+            r'accept': r'application/json',
+            r'Authorization':AUTH,            
+                        }
+        self.p_headers = {            
+            r'accept': r'*/*',
+            r'Authorization':AUTH,            
+            r'Content-Type': r'application/json'
+                        }
+
+
+    def test_add_list_success(self):
+        '''
+        Expected to successfully add a new user, return in list        
+        '''
+        print ('/---------------------------------/')
+        print ('Test Case: User Leaderboard - test_add_list_success')        
+        
+        # generate random name
+        __random_number = int(time.time())
+                                                    
+        self.body['username']= r'LY'+ str(__random_number)
+        self.body['score']=__random_number                             
+        
+        expected_code = r'201'
+        #expected_res = r'success'
+        
+        r = requests.post(USER_URL, data=json.dumps(self.body), headers=self.p_headers)                        
+        assert str(r).lower().find(expected_code)>=0, "Error - Failed to create a new user! Response=" + str(r)
+        
+        # check if new user can be returned in list
+        expected_code = r'200'
+        __headers = {}
+        r = requests.get(USER_URL, data=json.dumps(__headers), headers=self.p_headers)                        
+        assert str(r).lower().find(expected_code)>=0, "Error - Failed to fetch the user list! Response=" + str(r)
+                
+        # verify if new user name show in list
+        __expected_res = str(self.body['username']).lower()
+        assert str(r.content).lower().find(__expected_res)>=0, "Error - New user is not returned! Response=" + str(r.content)      
+        # verify if user's score is correct
+        __expected_res = str(__random_number)
+        assert str(r.content).find(__expected_res)>=0, "Error - New user's score is not returned! Response=" + str(r.content)
+
+
+    def test_add_fail_no_score(self):
+        '''
+        Expected not to allow adding a new user with no score        
+        '''
+        print ('/---------------------------------/')
+        print ('Test Case: User Leaderboard - test_add_success_no_score')        
+                
+        # generate random name
+        __random_number = int(time.time())
+                                                    
+        self.body['username']= r'LY'+ str(__random_number)
+        self.body['score']=''                             
+        
+        expected_code = r'400'
+        #expected_res = r'Error'
+        
+        r = requests.post(USER_URL, data=json.dumps(self.body), headers=self.p_headers)                        
+        assert str(r).lower().find(expected_code)>=0, "Error - should not allow adding a new user with no score! Response=" + str(r)
+        
+
+    def test_add_fail_empty_name(self):
+        '''
+        Expected not to allow adding a new user with empty username        
+        '''
+        print ('/---------------------------------/')
+        print ('Test Case: User Leaderboard - test_add_fail_empty_name')
+                
+                                                    
+        self.body['username']= ''
+        self.body['score']=123                             
+        
+        expected_code = r'400'        
+        
+        r = requests.post(USER_URL, data=json.dumps(self.body), headers=self.p_headers)                        
+        assert str(r).lower().find(expected_code)>=0, "Error - should not allow adding a new user with empty username! Response=" + str(r)
+
+    def test_add_fail_duplicate_name_case_insensitive(self):
+        '''
+        Expected not to allow adding a new user with same username but upper/lower cases        
+        '''
+        print ('/---------------------------------/')
+        print ('Test Case: User Leaderboard - test_add_fail_duplicate_name_case_insensitive')
+        
+        # add a new user first
+        __random_number = int(time.time())                                                    
+        self.body['username']= r'LY'+ str(__random_number)                
+        self.body['score']=1                             
+        
+        expected_code = r'201'                
+        r = requests.post(USER_URL, data=json.dumps(self.body), headers=self.p_headers)                        
+        assert str(r).lower().find(expected_code)>=0, "Error - Failed to add a new user! Response=" + str(r)
+        
+        # add the same name with lower case
+        self.body['username']= r'ly'+ str(__random_number)                                                      
+        expected_code = r'400'                
+        r = requests.post(USER_URL, data=json.dumps(self.body), headers=self.p_headers)                        
+        assert str(r).lower().find(expected_code)>=0, "Error - should not allow adding a new user with same username but upper/lower cases! Response=" + str(r)
+
+    def test_add_fail_duplicate_name(self):
+        '''
+        Expected not to allow adding a new user with a username already exists        
+        '''
+        print ('/---------------------------------/')
+        print ('Test Case: User Leaderboard - test_add_fail_duplicate_name')        
+        
+        self.body['username']= 'LY20211212'
+        self.body['score']=123                             
+        
+        expected_code = r'400'
+        #expected_res = r'error: duplicate key value violates unique constraint'        
+        
+        r = requests.post(USER_URL, data=json.dumps(self.body), headers=self.p_headers)                        
+        assert str(r).lower().find(expected_code)>=0, "Error - should not allow adding a new user with same username! Response=" + str(r)        
+        
+    def test_add_fail_long_name_51(self):
+        '''
+        Expected not to allow adding a new user with 51 charactors in username        
+        '''
+        print ('/---------------------------------/')
+        print ('Test Case: User Leaderboard - test_add_fail_long_name_51')
+        
+        self.body['username']= r'LY'+str(int(time.time())).zfill(49)
+        self.body['score']=123                             
+        
+        expected_code = r'400'                
+        
+        r = requests.post(USER_URL, data=json.dumps(self.body), headers=self.p_headers)                        
+        assert str(r).lower().find(expected_code)>=0, "Error - should not allow adding a new user with more than 51 charactors in name! Response=" + str(r)  
+    
+    def test_add_fail_score_letter(self):
+        '''
+        Expected not to allow adding a new user with letter in score        
+        '''
+        print ('/---------------------------------/')
+        print ('Test Case: User Leaderboard - test_add_fail_score_letter')
+        
+        # generate random name
+        __random_number = int(time.time())
+                                                    
+        self.body['username']= r'LY'+ str(__random_number)
+        self.body['score']='A'                             
+        
+        expected_code = r'400'        
+        
+        r = requests.post(USER_URL, data=json.dumps(self.body), headers=self.p_headers)                        
+        assert str(r).lower().find(expected_code)>=0, "Error - should not allow adding a new user with score 'A'! Response=" + str(r)
+                
+
+    def test_add_fail_score_minus(self):
+        '''
+        Expected not to allow adding a new user with minus score        
+        '''
+        print ('/---------------------------------/')
+        print ('Test Case: User Leaderboard - test_add_fail_score_minus')
+        
+        # generate random name
+        __random_number = int(time.time())
+                                                    
+        self.body['username']= r'LY'+ str(__random_number)
+        self.body['score']=-10                             
+        
+        expected_code = r'400'        
+        
+        r = requests.post(USER_URL, data=json.dumps(self.body), headers=self.p_headers)                        
+        assert str(r).lower().find(expected_code)>=0, "Error - should not allow adding a new user with minus score! Response=" + str(r)
+                
+            
+    def test_add_fail_large_score_2147483648(self):
+        '''
+        Expected not to allow adding a user with score to more than integer supported        
+        '''
+        print ('/---------------------------------/')
+        print ('Test Case: User Leaderboard - test_add_fail_large_score_2147483648')
+        
+        # generate random name
+        __random_number = int(time.time())
+                                                    
+        self.body['username']= r'LY'+ str(__random_number)
+        self.body['score']=2147483648                             
+        
+        expected_code = r'400'        
+        
+        r = requests.post(USER_URL, data=json.dumps(self.body), headers=self.p_headers)                        
+        assert str(r).lower().find(expected_code)>=0, "Error - should not allow adding a new user with score 2147483648! Response=" + str(r)    
+    
+    
+    def test_update_list_success(self):
+        '''
+        Expected to successfully update a user, and return in list        
+        '''
+        print ('/---------------------------------/')
+        print ('Test Case: User Leaderboard - test_update_list_success')        
+        
+        # generate random name
+        __random_number = int(time.time())
+                                                    
+        self.body['username']= r'LY20211212'
+        self.body['score']=__random_number                             
+        
+        expected_code = r'204'
+        #expected_res = r'success'
+        
+        r = requests.put(USER_URL, data=json.dumps(self.body), headers=self.p_headers)                        
+        assert str(r).lower().find(expected_code)>=0, "Error - Failed to update user's score! Response=" + str(r)
+        
+        # check if new user can be returned in list
+        expected_code = r'200'
+        __headers = {}
+        r = requests.get(USER_URL, data=json.dumps(__headers), headers=self.p_headers)                        
+        assert str(r).lower().find(expected_code)>=0, "Error - Failed to fetch the user list! Response=" + str(r)
+                              
+        # verify if user's score is correct
+        __expected_res = str(__random_number)
+        assert str(r.content).find(__expected_res)>=0, "Error - user's score is not updated! Response=" + str(r.content)
+
+
+    def test_update_new_user_success(self):
+        '''
+        Expected to successfully add a new user as username cannot be found        
+        '''
+        print ('/---------------------------------/')
+        print ('Test Case: User Leaderboard - test_update_new_user_success')        
+        
+        # generate random name
+        __random_number = int(time.time())
+                                                    
+        self.body['username']= r'LY20211212'+str(__random_number)
+        self.body['score']=__random_number                             
+        
+        expected_code = r'201'
+        #expected_res = r'success'
+        
+        r = requests.put(USER_URL, data=json.dumps(self.body), headers=self.p_headers)                        
+        assert str(r).lower().find(expected_code)>=0, "Error - Failed to add user when update cannot find the matched username! Response=" + str(r)
+        
+        # check if new user can be returned in list
+        expected_code = r'200'
+        __headers = {}
+        r = requests.get(USER_URL, data=json.dumps(__headers), headers=self.p_headers)                        
+        assert str(r).lower().find(expected_code)>=0, "Error - Failed to fetch the user list! Response=" + str(r)
+                
+        # verify if new user name show in list
+        __expected_res = str(self.body['username']).lower()
+        assert str(r.content).lower().find(__expected_res)>=0, "Error - New user is not returned! Response=" + str(r.content)      
+        # verify if user's score is correct
+        __expected_res = str(__random_number)
+        assert str(r.content).find(__expected_res)>=0, "Error - New user's score is not returned! Response=" + str(r.content)
+
+
+    def test_update_fail_no_score(self):
+        '''
+        Expected not to allow updating a user with no score        
+        '''
+        print ('/---------------------------------/')
+        print ('Test Case: User Leaderboard - test_update_fail_no_score')        
+                        
+        self.body['username']= r'LY20211212'
+        self.body['score']=''                             
+        
+        expected_code = r'400'
+        #expected_res = r'Error'
+        
+        r = requests.put(USER_URL, data=json.dumps(self.body), headers=self.p_headers)                        
+        assert str(r).lower().find(expected_code)>=0, "Error - should not allow updating a user with no score! Response=" + str(r)
+        
+
+    def test_update_fail_empty_name(self):
+        '''
+        Expected not to allow update with empty username        
+        '''
+        print ('/---------------------------------/')
+        print ('Test Case: User Leaderboard - test_update_fail_empty_name')
+                
+                                                    
+        self.body['username']= ''
+        self.body['score']=123                             
+        
+        expected_code = r'400'        
+        
+        r = requests.put(USER_URL, data=json.dumps(self.body), headers=self.p_headers)                        
+        assert str(r).lower().find(expected_code)>=0, "Error - should not allow updating a user with empty username! Response=" + str(r)
+
+    def test_update_sucess_name_case_insensitive(self):
+        '''
+        Expected to update existing user's score, even given username has different upper/lower cases        
+        '''
+        print ('/---------------------------------/')
+        print ('Test Case: User Leaderboard - test_update_sucess_name_case_insensitive')
+        
+        # add a new user first
+        __random_number_1 = int(time.time())                                                    
+        self.body['username']= r'LY'+ str(__random_number_1)                
+        self.body['score']=__random_number_1                             
+        
+        expected_code = r'201'                
+        r = requests.post(USER_URL, data=json.dumps(self.body), headers=self.p_headers)                        
+        assert str(r).lower().find(expected_code)>=0, "Error - Failed to add a new user! Response=" + str(r)
+        
+        # Update the same name with lower case
+        self.body['username']= r'ly'+ str(__random_number_1)
+        __random_number_2 = int(time.time())
+        self.body['score']=__random_number_2
+                                                              
+        expected_code = r'204'                
+        r = requests.put(USER_URL, data=json.dumps(self.body), headers=self.p_headers)                        
+        assert str(r).lower().find(expected_code)>=0, "Error - should not allow adding a new user with same username but upper/lower cases! Response=" + str(r)                  
+    
+    def test_update_fail_score_letter(self):
+        '''
+        Expected not to allow updating score to a letter        
+        '''
+        print ('/---------------------------------/')
+        print ('Test Case: User Leaderboard - test_update_fail_score_letter')
+                                
+        self.body['username']= r'LY20211212'
+        self.body['score']='A'                             
+        
+        expected_code = r'400'        
+        
+        r = requests.put(USER_URL, data=json.dumps(self.body), headers=self.p_headers)                        
+        assert str(r).lower().find(expected_code)>=0, "Error - should not allow change score to 'A'! Response=" + str(r)
+                
+
+    def test_update_fail_score_minus(self):
+        '''
+        Expected not to allow changing score to minus number        
+        '''
+        print ('/---------------------------------/')
+        print ('Test Case: User Leaderboard - test_update_fail_score_minus')
+                
+        self.body['username']= r'LY20211212'
+        self.body['score']=-10                             
+        
+        expected_code = r'400'        
+        
+        r = requests.put(USER_URL, data=json.dumps(self.body), headers=self.p_headers)                        
+        assert str(r).lower().find(expected_code)>=0, "Error - should not allow changing score to minus number! Response=" + str(r)
+                
+            
+    def test_update_fail_large_score_2147483648(self):
+        '''
+        Expected not to allow change score to more than integer supported        
+        '''
+        print ('/---------------------------------/')
+        print ('Test Case: User Leaderboard - test_add_fail_large_score_2147483648')
+        
+        self.body['username']= r'LY20211212'
+        self.body['score']=2147483648                             
+        
+        expected_code = r'400'        
+        
+        r = requests.put(USER_URL, data=json.dumps(self.body), headers=self.p_headers)                        
+        assert str(r).lower().find(expected_code)>=0, "Error - should not allow changing score to 2147483648! Response=" + str(r)
+
+                            
     def tearDown(self):
         pass
 
